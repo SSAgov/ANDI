ab3e1e6a977d932a55660ee1800aeeab4a1ba3ae
Ollie Willcox <owillcox@iteratorstesting.com>
2021-05-17 17:28:41 -0400

    Added some start to using opacity in color contrast

--- andi/candi.js
+++ andi/candi.js
@@ -54,6 +54,16 @@ function init_module() {
         this.elementsWithTextCount = 0;
     }
 
+    function rgba2rgb(RGB_background, RGBA_color) {
+    var alpha = RGBA_color.a;
+
+    return new Color(
+        (1 - alpha) * RGB_background.r + alpha * RGBA_color.r,
+        (1 - alpha) * RGB_background.g + alpha * RGBA_color.g,
+        (1 - alpha) * RGB_background.b + alpha * RGBA_color.b
+    );
+}
+
     AndiModule.initActiveActionButtons({
         contrastPlayground: false,
         grayscale: false
