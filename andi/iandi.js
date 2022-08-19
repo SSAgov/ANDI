e01e5714c3515c1e077567d960f206dfe004164b
Cotter, John <john.cotter@ssa.gov>
2022-08-19 14:26:17 -0400

    github issue #133

--- andi/iandi.js
+++ andi/iandi.js
@@ -1,5 +1,5 @@
 //=============================================//
-//fANDI: focusable elements ANDI (default mode)//
+//iANDI: iframe ANDI						   //
 //Created By Social Security Administration	   //
 //=============================================//
 function init_module(){
@@ -9,7 +9,7 @@ var iandiVersionNumber = "3.0.1";
 //create iANDI instance
 var iANDI = new AndiModule(iandiVersionNumber,"i");
 
-//This function will analyze the test page for focusable element related markup relating to accessibility
+//This function will analyze the test page for iframes
 iANDI.analyze = function(){
 	$(TestPageData.allVisibleElements).each(function(){
 		if($(this).is("iframe")){
@@ -24,11 +24,11 @@ iANDI.analyze = function(){
 //Inserts some counter totals, displays the accesskey list
 iANDI.results = function(){
 	andiBar.updateResultsSummary("Iframes with Content: "+testPageData.andiElementIndex);
-	
+
 	if(testPageData.andiElementIndex > 0){
 		var iframesSelectionMenu = "";
 		var iframesSelectionLinks = "";
-		
+
 		$("#ANDI508-testPage .ANDI508-element").each(function(){
 			//Build iFrame List
 			iframesSelectionLinks += "<li><a href='javascript:void(0)' data-andi508-relatedindex='"+$(this).attr('data-andi508-index')+"'>";
@@ -43,15 +43,15 @@ iANDI.results = function(){
 			iframesSelectionMenu += "<p>Select iframe to open in a new tab, then launch ANDI.</p>"+
 			"<ol>" + iframesSelectionLinks + "</ol>";
 		}
-		
+
 		$("#ANDI508-additionalPageResults").append("<button id='ANDI508-viewIframeList-button' class='ANDI508-viewOtherResults-button' aria-expanded='false'>"+listIcon+"view iframe list</button><div id='iANDI508-iframeList-container' class='ANDI508-viewOtherResults-expanded' tabindex='0'><div class='ANDI508-scrollable'>"+iframesSelectionMenu+"</div></div>");
-		
+
 		$("#iANDI508-iframeList-container").find("a").click(function(){
 			var relatedIndex = $(this).attr("data-andi508-relatedindex");
 			var relatedIframe = $("#ANDI508-testPage .ANDI508-element[data-andi508-index="+relatedIndex+"]");
 			iANDI.openIframeInNewWindow(relatedIframe);
 		});
-		
+
 		//Define outline button
 		$("#ANDI508-viewIframeList-button").click(function(){
 			if($(this).attr("aria-expanded") === "true"){
@@ -69,7 +69,7 @@ iANDI.results = function(){
 			else{
 				//show iframe list, hide alert list
 				$("#ANDI508-alerts-list").hide();
-				
+
 				andiSettings.minimode(false);
 				$(this)
 					.html(listIcon+"hide iframe list")
@@ -81,8 +81,8 @@ iANDI.results = function(){
 			andiResetter.resizeHeights();
 			return false;
 		});
-		
-		//For iframe list links, add hoverability, focusability, clickability 
+
+		//For iframe list links, add hoverability, focusability, clickability
 		$("#iANDI508-iframeList-container").find("a[data-andi508-relatedindex]").each(function(){
 			andiFocuser.addFocusClick($(this));
 			var relatedIndex = $(this).attr("data-andi508-relatedindex");
@@ -97,16 +97,16 @@ iANDI.results = function(){
 				AndiModule.inspect(relatedElement[0]);
 			});
 		});
-		
+
 		andiBar.showStartUpSummary("To test the contents of <span class='ANDI508-module-name-i'>iframes</span>, each must be viewed independently.<br />Inspect an iframe, press the \"test in new tab\" button, then launch ANDI.", true);
-		
+
 	}
 	else{
 		andiBar.showStartUpSummary("No visible <span class='ANDI508-module-name-i'>iframes</span>.");
 	}
-	
+
 	andiAlerter.updateAlertList();
-	
+
 	$("#ANDI508").focus();
 };
 
@@ -119,10 +119,10 @@ AndiModule.inspect = function(element){
 	var addOnProps = AndiData.getAddOnProps(element, elementData,
 		["src"]
 	);
-		
+
 	andiBar.displayOutput(elementData, element, addOnProps);
 	andiBar.displayTable(elementData, element, addOnProps);
-	
+
 	$("#ANDI508-additionalElementDetails").html("<button>test in new tab</button>");
 	$("#ANDI508-additionalElementDetails button").click(function(){
 		iANDI.openIframeInNewWindow(element);
@@ -130,7 +130,7 @@ AndiModule.inspect = function(element){
 	});
 };
 
-//This function will open an iframe in a new window 
+//This function will open an iframe in a new window
 iANDI.openIframeInNewWindow = function(iframe, src){
 	var iframeWindow;
 	var url = $(iframe).attr("src");
